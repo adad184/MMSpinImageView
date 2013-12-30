dcefcf1bdd47d1e01ebc983518c47e8d6465b4c7
adad184 <adad184@gmail.com>
2013-12-30 09:06:03 +0800

    initial commit

---
+++ MMSpinImageView/MMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MMAppDelegate.h
+//  MMSpinImageView
+//
+//  Created by Ralph Li on 12/30/13.
+//  Copyright (c) 2013 LJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MMSpinImageView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MMSpinImageView
+//
+//  Created by Ralph Li on 12/30/13.
+//  Copyright (c) 2013 LJC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MMAppDelegate class]));
+    }
+}
